8eaa13df56a365e83b4e1bc92560977ed2412f73
domfernando319 <domfernando319@gmail.com>
2024-07-07 21:35:11 -0400

    Direct messaging and chatting with other users.

--- socialnet/src/router/index.js
+++ socialnet/src/router/index.js
@@ -3,12 +3,11 @@ import HomeView from '../views/HomeView.vue'
 import SignUpView from '../views/SignUpView.vue'
 import LoginView from '../views/LoginView.vue'
 import FeedView from '../views/FeedView.vue'
-import MessagesView from '../views/MessagesView.vue'
 import SearchView from '../views/SearchView.vue'
 import ProfileView from '../views/ProfileView.vue'
 import FriendsView from '../views/FriendsView.vue'
 import PostView from '../views/PostView.vue'
-
+import ChatView from '../views/ChatView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -42,11 +41,6 @@ const router = createRouter({
       component: FeedView
     },
     {
-      path: '/messages',
-      name: 'messages',
-      component: MessagesView
-    },
-    {
       path: '/search',
       name: 'search',
       component: SearchView
@@ -66,6 +60,12 @@ const router = createRouter({
       name: 'postview',
       component: PostView
     },
+    {
+      path: '/chat',
+      name: 'chat',
+      component: ChatView
+    },
+    
   ]
 })
 
